ac6e4d72c6efaf03ac3a4bf4cd67dce93226b6cf
Dmitry Vlasenko <57891870+Dmitry-Vlasenko@users.noreply.github.com>
2024-03-26 21:32:09 +0200

    refactoring code

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,7 +5,6 @@ import app.models.BinaryTree;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
